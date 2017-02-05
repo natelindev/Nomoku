2b898c9c34465314fd2951b7c9c116bb967d48b0
Nathaniel <linlilulll@gmail.com>
2017-02-05 11:15:51 +0800

    Inital commit

---
+++ Nomoku.cpp
@@ -0,0 +1,123 @@
+#include "pisqpipe.h"
+#include <windows.h>
+
+const char *infotext = "name=\"Nomuku\", author=\"Nathaniel Lin & Howard Liu\", version=\"0.1 pre alpha\", country=\"China\", www=\"http://blog.nathaniel-lin.me";
+
+#define MAX_BOARD 100
+int board[MAX_BOARD][MAX_BOARD];
+static unsigned seed;
+
+
+void brain_init()
+{
+	if (width<5 || height<5){
+		pipeOut("ERROR size of the board");
+		return;
+	}
+	if (width>MAX_BOARD || height>MAX_BOARD){
+		pipeOut("ERROR Maximal board size is %d", MAX_BOARD);
+		return;
+	}
+	seed = start_time;
+	pipeOut("OK");
+}
+
+void brain_restart()
+{
+	int x, y;
+	for (x = 0; x<width; x++){
+		for (y = 0; y<height; y++){
+			board[x][y] = 0;
+		}
+	}
+	pipeOut("OK");
+}
+
+int isFree(int x, int y)
+{
+	return x >= 0 && y >= 0 && x<width && y<height && board[x][y] == 0;
+}
+
+void brain_my(int x, int y)
+{
+	if (isFree(x, y)){
+		board[x][y] = 1;
+	}
+	else{
+		pipeOut("ERROR my move [%d,%d]", x, y);
+	}
+}
+
+void brain_opponents(int x, int y)
+{
+	if (isFree(x, y)){
+		board[x][y] = 2;
+	}
+	else{
+		pipeOut("ERROR opponents's move [%d,%d]", x, y);
+	}
+}
+
+void brain_block(int x, int y)
+{
+	if (isFree(x, y)){
+		board[x][y] = 3;
+	}
+	else{
+		pipeOut("ERROR winning move [%d,%d]", x, y);
+	}
+}
+
+int brain_takeback(int x, int y)
+{
+	if (x >= 0 && y >= 0 && x<width && y<height && board[x][y] != 0){
+		board[x][y] = 0;
+		return 0;
+	}
+	return 2;
+}
+
+unsigned rnd(unsigned n)
+{
+	seed = seed * 367413989 + 174680251;
+	return (unsigned)(UInt32x32To64(n, seed) >> 32);
+}
+
+void brain_turn()
+{
+	int x, y, i;
+
+	i = -1;
+	do{
+		x = rnd(width);
+		y = rnd(height);
+		i++;
+		if (terminateAI) return;
+	} while (!isFree(x, y));
+
+	if (i>1) pipeOut("DEBUG %d coordinates didn't hit an empty field", i);
+	do_mymove(x, y);
+}
+
+void brain_end()
+{
+}
+
+#ifdef DEBUG_EVAL
+#include <windows.h>
+
+void brain_eval(int x, int y)
+{
+	HDC dc;
+	HWND wnd;
+	RECT rc;
+	char c;
+	wnd = GetForegroundWindow();
+	dc = GetDC(wnd);
+	GetClientRect(wnd, &rc);
+	c = (char)(board[x][y] + '0');
+	TextOut(dc, rc.right - 15, 3, &c, 1);
+	ReleaseDC(wnd, dc);
+}
+
+#endif
---
+++ readme.md
@@ -0,0 +1,9 @@
+## 使用方法
+
+1. 下载[piskvork](http://petr.lastovicka.sweb.cz/piskvork.zip)
+2. 在piskvork manager 里面选择你生成的exe文件
+3. 开始和你的AI比赛吧
+
+注意：你生成的exe文件必须以`pbrain-`为前缀，否则无法被piskvork识别
+
+另外，不要修改`pisqpipe.cpp`和`pisqpipe.h`，否则AI可能会无法运行
